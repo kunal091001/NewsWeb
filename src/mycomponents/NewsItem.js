f4be02521a83379994e3b1455a62012b0425e428
[kunal] <kunal9313882818@gmail.com>
2023-01-24 11:13:30 +0530

    some change

--- src/mycomponents/NewsItem.js
+++ src/mycomponents/NewsItem.js
@@ -7,7 +7,7 @@ export default function NewsItem(props) {
       <div className="card" style={{ backgroundColor: mode === 'light' ? 'white' : '#36454f', Color: mode === 'light' ? 'white' : '#212529' }}>
         <img src={imageUrl ? imageUrl : { Hello }} className="card-img-top " style={{ height: "200px" }} alt='News' />
         <div className="card-body" >
-          <div style={{ height: '100px' }}><h6 className="card-title">{title}</h6></div>
+          <h6 className="card-title">{title}</h6>
           <div className='mydiv' style={{ height: '25vh' }} ><p className="card-text para" >{description}</p></div>
           <p className='card-text'><small className='text-muted'>By {author} on {new Date(date).toGMTString()}</small></p>
           <a href={newsUrl} rel="noreferrer" target='_blank' className="btn btn-sm btn-primary">Read More</a>
